1fb1c8ff224abbf08bfcfe91e2f4d63c62f3b4b0
julio4ai <juliocolomer@gmail.com>
2024-02-02 11:21:25 +0100

    345

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from langchain.agents import (
 from langchain.prompts import StringPromptTemplate
 from langchain_openai import OpenAI
 from langchain.chains import LLMChain
-from langchain_community.utilities.SerpAPIWrapper import SerpAPIWrapper
+from langchain_community.utilities import SerpAPIWrapper
 from typing import List, Union
 from langchain.schema import AgentAction, AgentFinish
 from langchain_community.agent_toolkits import NLAToolkit
